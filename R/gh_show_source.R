fc96af9889b07925721471b36e591a510a205ff3
hoxo-m <hoxo.smile@gmail.com>
2016-05-13 19:33:49 +0900

    rename gh_find_func_definition -> gh_show_source

---
+++ R/gh_show_source.R
@@ -0,0 +1,50 @@
+#' Find source code for functions in packages on GitHub
+#' 
+#' @param func a function or a character as a function name
+#' @param repo a character as a GitHub repository name
+#' 
+#' @examples
+#' \donttest{
+#' gh_show_source("mutate", "dplyr")
+#' }
+#' 
+#' @importFrom jsonlite fromJSON
+#' @importFrom utils browseURL
+#' 
+#' @export
+gh_show_source <- function(func, repo = NULL) {
+  if(is.character(func)) {
+    func_name <- func
+  } else {
+    func_name <- substitute(func)
+  }
+  if(is.null(repo)) {
+    func <- match.fun(func)
+    ns <- environment(func)
+    package_name <- get(".packageName", ns)
+    repo_name <- select_repository(package_name)
+  } else {
+    repo_name <- select_repository(repo)
+  }
+  
+  contents_url <- sprintf("https://api.github.com/repos/%s/contents/R", repo_name)
+  download_urls <- fromJSON(contents_url)$download_url
+
+  found <- FALSE
+  for(url in download_urls) {
+    message("checking ", basename(url))
+    names <- sapply(parse(url), function(x) x[[2]])
+    if(func_name %in% names) {
+      found <- TRUE
+      break
+    }
+  }
+
+  if(found) {
+    line_num <- which(grepl(paste0(func_name, "<-"), gsub("\\s", "", readLines(url))))
+    url <- sprintf("https://github.com/%s/tree/master/R/%s#L%d", repo_name, basename(url), line_num)
+    browseURL(url)
+  } else {
+    stop("not found")
+  }
+}
