3fcac0694da122d9de4286f194ab19b5a5c400ea
hoxo-m <hoxo.smile@gmail.com>
2016-05-28 19:57:59 +0900

    fix

--- tests/testthat/test-gh_install_packages.R
+++ tests/testthat/test-gh_install_packages.R
@@ -1,20 +1,20 @@
 context("Install packages")
 Sys.setenv("R_TESTS" = "")
 tmp <- file.path(tempdir(), "tmplib")
-suppressWarnings(dir.create(tmp))
+suppressWarnings(dir.create(tmp, mode = as.octmode("777")))
 
 test_that("Install a single package", {
   repo <- "AnomalyDetection"
   
-  act <- suppressWarnings(gh_install_packages(repo, lib=tmp))
+  act <- suppressWarnings(gh_install_packages(repo, lib = tmp))
   expect_false(is.na(act))
   remove.packages("AnomalyDetection", lib = tmp)
 })
 
-test_that("Install tow packages", {
+test_that("Install two packages", {
   repo <- c("AnomalyDetection", "densratio")
   
-  act <- suppressWarnings(gh_install_packages(repo, build_vignettes = FALSE, lib=tmp))
+  act <- suppressWarnings(gh_install_packages(repo, build_vignettes = FALSE, lib = tmp))
   expect_equal(length(act), 2)
   expect_false(is.na(act[1]))
   expect_false(is.na(act[2]))
