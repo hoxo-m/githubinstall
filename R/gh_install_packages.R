a964c8728307d2c5636c87f08f20e65308180688
hoxo-m <hoxo.smile@gmail.com>
2016-07-19 00:04:10 +0900

    fix

--- R/gh_install_packages.R
+++ R/gh_install_packages.R
@@ -48,11 +48,7 @@ gh_install_packages <- function(packages, ask = TRUE, ref = "master",
   
   # Confirm to install
   if (ask) {
-    if (length(repos) == 1) {
-      target <- paste0(repos, "  (", titles, ")", collapse = "\n - ")
-    } else {
-      target <- paste0(format_choices(repos), collapse = "\n - ")
-    }
+    target <- paste0(format_choices(repos), collapse = "\n - ")
     msg <- sprintf("Suggestion:\n - %s", target)
     message(msg)
     prompt <- sprintf("Do you want to install the package%s (Y/n)?  ", ifelse(length(repos) == 1, "", "s"))
@@ -102,6 +98,43 @@ select_dependencies <- function(ask, build_vignettes, dependencies, quiet) {
   dependencies
 }
 
+
+#' @importFrom devtools github_pull
+#' @importFrom stringr str_replace str_sub
+separate_reference <- function(packages, ref) {
+  commit_pattern <- "@.+$"
+  pull_request_pattern <- "#.+$"
+  branch_pattern <- "\\[.+\\]$"
+  
+  commit <- vapply(packages, extract_reference, character(1), commit_pattern)
+  pull_request <- vapply(packages, extract_reference, character(1), pull_request_pattern)
+  branch <- vapply(packages, extract_reference, character(1), branch_pattern)
+  
+  commit <- str_sub(commit, 2)
+  pull_request <- github_pull(str_sub(pull_request, 2))
+  branch <- str_sub(branch, 2, -2)
+  
+  if (length(ref) == 1)
+    ref <- rep(ref, length(packages))
+  
+  references <- commit
+  references[is.na(references)] <- pull_request[is.na(references)]
+  references[is.na(references)] <- branch[is.na(references)]
+  references[is.na(references)] <- ref[is.na(references)]
+  
+  packages <- str_replace(packages, references, "")
+  list(packages = packages, references = references)
+}
+
+#' @importFrom stringr str_detect str_extract
+extract_reference <- function(x, pattern) {
+  if (str_detect(x, pattern)) {
+    str_extract(x, pattern)
+  } else {
+    NA_character_
+  }
+}
+
 #' @importFrom utils menu
 select_repository <- function(package_name) {
   candidates <- gh_suggest(package_name, keep_title = TRUE)
@@ -125,5 +158,50 @@ format_choices <- function(candidates) {
   nchars <- nchar(candidates)
   max_nchars <- max(nchars)
   spaces <- sapply(max_nchars - nchars, function(n) paste(rep(" ", n + 1), collapse=""))
-  paste0(candidates, spaces, attr(candidates, "title"))
+  paste0(candidates, spaces, " ", attr(candidates, "title"))
+}
+
+#' @importFrom utils packageDescription
+remove_conflict_repos <- function(repos, lib, quiet, ask) {
+  ignored_inds <- c()
+  
+  splitted <- strsplit(repos, "/")
+  usernames <- sapply(splitted, function(x) x[1])
+  package_names <- sapply(splitted, function(x) x[2])
+  descs <- lapply(package_names, function(pkg) suppressWarnings(packageDescription(pkg, lib.loc = lib)))
+  inds <- which(!is.na(descs))
+  
+  for (i in inds) {
+    message <- NULL
+    username <- usernames[i]
+    package_name <- package_names[i]
+    desc <- descs[[i]]
+    if(exists("GithubRepo", where = desc)) {
+      repo <- repos[i]
+      if (username != desc$GithubUsername) {
+        installed_repo <- paste0(desc$GithubUsername, "/", desc$GithubRepo)
+        message <- sprintf('Installing "%s", but already installed "%s".', repo, installed_repo)
+      }
+    } else  {
+      package_repository <- ifelse(exists("Repository", where = desc), desc$Repository, "unknown repository")
+      message <- sprintf('Installing "%s" package from GitHub, but already installed from %s.', package_name, package_repository)
+    }
+    if (!quiet & !is.null(message)) {
+      message(message)
+      if (ask) {
+        prompt <- "Do you want to overwrite the package (Y/n)?  "
+        answer <- substr(readline(prompt), 1L, 1L)
+        if (!(answer %in% c("", "y", "Y"))) {
+          ignored_inds <- c(ignored_inds, i)
+        }
+      } else {
+        message("It will be overwriten.")
+      }
+    }
+  }
+  if (is.null(ignored_inds)) {
+    repos
+  } else {
+    repos[-ignored_inds]
+  }
 }
--- R/reserve_suffix.R
+++ R/reserve_suffix.R
@@ -1,97 +1,63 @@
-SUFFIX <- "suffix"
-SUBDIR <- "subdir"
-
-#' @importFrom devtools github_pull
-#' @importFrom stringr str_replace str_sub
-separate_reference <- function(packages, ref) {
-  commit_pattern <- "@.+$"
-  pull_request_pattern <- "#.+$"
-  branch_pattern <- "\\[.+\\]$"
-
-  commit <- vapply(packages, extract_reference, character(1), commit_pattern)
-  pull_request <- vapply(packages, extract_reference, character(1), pull_request_pattern)
-  branch <- vapply(packages, extract_reference, character(1), branch_pattern)
-  
-  commit <- str_sub(commit, 2)
-  pull_request <- github_pull(str_sub(pull_request, 2))
-  branch <- str_sub(branch, 2, -2)
-  
-  if (length(ref) == 1)
-    ref <- rep(ref, length(packages))
-  
-  references <- commit
-  references[is.na(references)] <- pull_request[is.na(references)]
-  references[is.na(references)] <- branch[is.na(references)]
-  references[is.na(references)] <- ref[is.na(references)]
-  
-  packages <- str_replace(packages, references, "")
-  list(packages = packages, references = references)
-}
-
-#' @importFrom stringr str_detect str_extract
-extract_reference <- function(x, pattern) {
-  if (str_detect(x, pattern)) {
-    str_extract(x, pattern)
-  } else {
-    NA_character_
-  }
-}
-
-reserve_suffix <- function(packages) {
-  commit_pattern <- "@.+$"
-  pull_request_pattern <- "#.+$"
-  branch_pattern <- "\\[.+\\]$"
-  
-  packages <- lapply(packages, function(x) reserve_to_attr(x, commit_pattern, SUFFIX))
-  packages <- lapply(packages, function(x) reserve_to_attr(x, pull_request_pattern, SUFFIX))
-  packages <- lapply(packages, function(x) reserve_to_attr(x, branch_pattern, SUFFIX))
-  attrs <- unlist(lapply(packages, function(x) attr(x, SUFFIX)))
-  packages <- unlist(packages)
-  attr(packages, SUFFIX) <- attrs
-  
-  packages
-}
-
-#' @importFrom stringr str_detect str_extract str_replace
-reserve_to_attr <- function(x, pattern, attr_name) {
-  if (str_detect(x, pattern)) {
-    attr <- str_extract(x, pattern)
-    x <- str_replace(x, pattern, "")
-    attr(x, attr_name) <- attr
-  } else if (is.null(attr(x, attr_name))) {
-    attr(x, attr_name) <- ""
-  }
-  x
-}
-
-reserve_subdir <- function(packages) {
-  suffix <- attr(packages, SUFFIX)
-  packages <- extract_repositry_name_with_subdir(packages)
-  attr(packages, SUFFIX) <- suffix
-  packages
-}
-
-#' @importFrom stringr str_extract str_replace
-extract_repositry_name_with_subdir <- function(packages) {
-  repositry_name_pattern <- "^[^/]+/[^/]+"
-  result <- lapply(packages, function(x) {
-    if (is_contain_subdir(x)) {
-      repositry_name <- str_extract(x, repositry_name_pattern)
-      attr <- str_replace(x, repositry_name, "")
-      attr(repositry_name, SUBDIR) <- attr
-      repositry_name
-    } else {
-      attr(x, SUBDIR) <- ""
-      x
-    }
-  })
-  attr <- sapply(result, function(x) attr(x, SUBDIR))
-  result <- unlist(result)
-  attr(result, SUBDIR) <- attr
-  result
-}
-
-#' @importFrom stringr str_count
-is_contain_subdir <- function(packages) {
-  str_count(packages, "/") >= 2
-}
+#' SUFFIX <- "suffix"
+#' SUBDIR <- "subdir"
+#' 
+#' 
+#' 
+#' reserve_suffix <- function(packages) {
+#'   commit_pattern <- "@.+$"
+#'   pull_request_pattern <- "#.+$"
+#'   branch_pattern <- "\\[.+\\]$"
+#'   
+#'   packages <- lapply(packages, function(x) reserve_to_attr(x, commit_pattern, SUFFIX))
+#'   packages <- lapply(packages, function(x) reserve_to_attr(x, pull_request_pattern, SUFFIX))
+#'   packages <- lapply(packages, function(x) reserve_to_attr(x, branch_pattern, SUFFIX))
+#'   attrs <- unlist(lapply(packages, function(x) attr(x, SUFFIX)))
+#'   packages <- unlist(packages)
+#'   attr(packages, SUFFIX) <- attrs
+#'   
+#'   packages
+#' }
+#' 
+#' #' @importFrom stringr str_detect str_extract str_replace
+#' reserve_to_attr <- function(x, pattern, attr_name) {
+#'   if (str_detect(x, pattern)) {
+#'     attr <- str_extract(x, pattern)
+#'     x <- str_replace(x, pattern, "")
+#'     attr(x, attr_name) <- attr
+#'   } else if (is.null(attr(x, attr_name))) {
+#'     attr(x, attr_name) <- ""
+#'   }
+#'   x
+#' }
+#' 
+#' reserve_subdir <- function(packages) {
+#'   suffix <- attr(packages, SUFFIX)
+#'   packages <- extract_repositry_name_with_subdir(packages)
+#'   attr(packages, SUFFIX) <- suffix
+#'   packages
+#' }
+#' 
+#' #' @importFrom stringr str_extract str_replace
+#' extract_repositry_name_with_subdir <- function(packages) {
+#'   repositry_name_pattern <- "^[^/]+/[^/]+"
+#'   result <- lapply(packages, function(x) {
+#'     if (is_contain_subdir(x)) {
+#'       repositry_name <- str_extract(x, repositry_name_pattern)
+#'       attr <- str_replace(x, repositry_name, "")
+#'       attr(repositry_name, SUBDIR) <- attr
+#'       repositry_name
+#'     } else {
+#'       attr(x, SUBDIR) <- ""
+#'       x
+#'     }
+#'   })
+#'   attr <- sapply(result, function(x) attr(x, SUBDIR))
+#'   result <- unlist(result)
+#'   attr(result, SUBDIR) <- attr
+#'   result
+#' }
+#' 
+#' #' @importFrom stringr str_count
+#' is_contain_subdir <- function(packages) {
+#'   str_count(packages, "/") >= 2
+#' }
