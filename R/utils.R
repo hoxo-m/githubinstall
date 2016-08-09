14e16212287240c8b99ef1eb18bc750b27adf05a
hoxo-m <hoxo.smile@gmail.com>
2016-08-09 22:43:12 +0900

    move get_package_list() to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+get_package_list <- function() {
+  if(!exists("package_list", envir = .options)) {
+    gh_update_package_list()
+  }
+  .options$package_list
+}
+
 stop_without_message <- function() {
   old_sem <- options("show.error.messages")$show.error.messages
   on.exit(options(show.error.messages = old_sem))
